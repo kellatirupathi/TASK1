9945bb5989ba9f66b5fc195c70b91d651efab97f
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2024-10-09 21:41:34 +0530

    Update cryptoController.js

--- src/controllers/cryptoController.js
+++ src/controllers/cryptoController.js
@@ -1,4 +1,25 @@
+const Crypto = require('../models/Crypto');
 const calculateStandardDeviation = require('../utils/standardDeviation');
+
+const getCryptoStats = async (req, res) => {
+  const { coin } = req.query;
+
+  try {
+    const latestData = await Crypto.findOne({ coin }).sort({ fetchedAt: -1 });
+
+    if (!latestData) {
+      return res.status(404).json({ message: 'Data not found' });
+    }
+
+    res.json({
+      price: latestData.price,
+      marketCap: latestData.marketCap,
+      "24hChange": latestData.change24h,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const getCryptoDeviation = async (req, res) => {
   const { coin } = req.query;
