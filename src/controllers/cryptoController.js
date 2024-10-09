e2d953f4eb1e0a67fefd9c17e064021ca2513357
KELLA TIRUPATHI RAO <106460293+kellatirupathi@users.noreply.github.com>
2024-10-09 21:35:56 +0530

    Update cryptoController.js

--- src/controllers/cryptoController.js
+++ src/controllers/cryptoController.js
@@ -1,20 +1,19 @@
-const Crypto = require('../models/Crypto');
+const calculateStandardDeviation = require('../utils/standardDeviation');
 
-const getCryptoStats = async (req, res) => {
+const getCryptoDeviation = async (req, res) => {
   const { coin } = req.query;
 
   try {
-    const latestData = await Crypto.findOne({ coin }).sort({ fetchedAt: -1 });
-
-    if (!latestData) {
-      return res.status(404).json({ message: 'Data not found' });
+    const dataRecords = await Crypto.find({ coin }).sort({ fetchedAt: -1 }).limit(100);
+    
+    if (dataRecords.length < 2) {
+      return res.status(400).json({ message: 'Not enough data for calculation' });
     }
 
-    res.json({
-      price: latestData.price,
-      marketCap: latestData.marketCap,
-      "24hChange": latestData.change24h,
-    });
+    const prices = dataRecords.map(record => record.price);
+    const deviation = calculateStandardDeviation(prices);
+
+    res.json({ deviation: deviation.toFixed(2) });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -22,4 +21,5 @@ const getCryptoStats = async (req, res) => {
 
 module.exports = {
   getCryptoStats,
+  getCryptoDeviation,
 };
